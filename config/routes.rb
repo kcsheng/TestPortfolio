8b7e02dffe4c914229688554f6eb9f494452c679
kcsheng <kacysheng@mail.com>
2020-06-24 14:34:58 +1000

    configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  
+  # customised-originally: get "pages/about"  
+  get "about-me", to: "pages#about" # set address as /about, not pages/about
+  # originally: get "pages/contact"
+  get "contact", to: "pages#contact"
+
+  root to: "pages#home" # move the home page to root, not the server welcome page
+  # use rails routes for any changes in routes.rb in config
+
   resources :blogs
 end
