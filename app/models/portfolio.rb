2eddd9a8ce137e84b6493ff822bc007961b9c070
kcsheng <kacysheng@mail.com>
2020-06-28 16:16:26 +1000

    implemented image defaults for portfolio-items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,15 @@ class Portfolio < ApplicationRecord
 
   # rails way of custom scope, using lambda
   # scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+
+  # another way of implementing custom routes with changeable default value
+  # After a new portfolio item is initialised, add image by default 
+  after_initialize :set_defaults
+  
+  def set_defaults 
+    self.main_image ||= "https://via.placeholder.com/600x400" # || will respect existing image, not overwrite it
+    self.thumb_image ||= "https://via.placeholder.com/350x200" # See || as an optional stop
+  end
 end
+
+
