d30e53986205b499503fbdbdf56d6eafb4cca452
kcsheng <kacysheng@mail.com>
2020-07-22 14:24:48 +1000

    Refacored application controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   include DeviseWhiteList
-
-  before_action :visit_from
-  def visit_from
-    session[:source] = params[:q] if params[:q]
-  end
-
+  include SetSource
 end
