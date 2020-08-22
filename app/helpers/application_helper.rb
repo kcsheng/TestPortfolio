73541a24f2801b1460741f62d01d8f59a7c65988
kcsheng <kacysheng@mail.com>
2020-08-22 15:50:13 +1000

    Implemented refactor for navigation and added active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,5 +17,40 @@ module ApplicationHelper
 
   def copyright_generator
     KcsViewTool::Renderer.copyright("KC Sheng", "All rights reserved")
+  end 
+  
+# Initial implementation of nav_helper
+#   def nav_helper(style, tag) ## Pick parameters that distinguish pages' styles. 
+# nav_links = <<LINKS ## heredoc - easy string interpolation (avoid single quotation marks)
+# <#{tag}><a href="#{root_path}" class="#{style} #{active_or_not(root_path)}">Home</a></#{tag}>
+# <#{tag}><a href="#{about_me_path}" class="#{style} #{active_or_not(about_me_path)}">About-me</a></#{tag}>
+# <#{tag}><a href="#{contact_path}" class="#{style} #{active_or_not(contact_path)}">Contact</a></#{tag}>
+# <#{tag}><a href="#{blogs_path}" class="#{style} #{active_or_not(blogs_path)}">Blogs</a></#{tag}>
+# <#{tag}><a href="#{portfolios_path}" class="#{style} #{active_or_not(portfolios_path)}">Portfolio</a></#{tag}>
+# LINKS
+#     nav_links.html_safe
+#   end
+
+  def nav_pages
+    { "Home" => root_path,
+      "About me" => about_me_path, 
+      "Contact" => contact_path, 
+      "Blogs" => blogs_path, 
+      "Portfolio" => portfolios_path
+    } 
   end
+
+  def active_or_not(path)
+    "active" if current_page?(path)
+  end
+
+  def nav_helper(style, tag)
+    nav_links = ""    
+    nav_pages.each do |page, path|
+      active_state = active_or_not(path)
+      nav_links << "<#{tag}><a href='#{path}' class='#{style} #{active_state}'>#{page}</a></#{tag}>"
+    end
+    nav_links.html_safe
+  end
+
 end
