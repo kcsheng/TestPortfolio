0fb1530b9a516adba2b6450cdaddb734536af325
kcsheng <kacysheng@mail.com>
2020-09-01 10:43:01 +1000

    Implemnted persistent position update for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,6 +6,13 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.by_position
   end
 
+  def sort 
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+    render nothing: true
+  end
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
@@ -21,7 +28,6 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        # redirect browser to the full list of Portfolio, same as: portfolios
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live!' }
       else
         format.html { render :new }
