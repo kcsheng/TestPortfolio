92e88668a572a909869730721daf0d7325296d65
kcsheng <kacysheng@mail.com>
2020-09-12 09:43:54 +1000

    Updated file upload forms and replace placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,8 @@ class Portfolio < ApplicationRecord
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs["name"].blank? }
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -16,18 +16,7 @@ class Portfolio < ApplicationRecord
   def self.by_position
     order("position ASC")
   end
-
   # rails way of custom scope, using lambda
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
-
-  # another way of implementing custom routes with changeable default value
-  # After a new portfolio item is initialised, add image by default 
-  after_initialize :set_defaults
-  
-  def set_defaults 
-    self.main_image ||= Placeholder.image_generator(height:"600", width:"400") # || will respect existing image, not overwrite it
-    self.thumb_image ||= Placeholder.image_generator(height:"350", width:"200") # See || as an optional stop
-  end
 end
-
 
