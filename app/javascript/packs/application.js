7c79d2b9f742e18e3589ab8a3cf190954b1685e0
kcsheng <kacysheng@mail.com>
2020-09-15 12:32:39 +1000

    Implemented gritter for notifications

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require("packs/html5sortable")
 require("packs/portfolios")
 
 require("@nathanvda/cocoon")
+require("gritter/js/jquery.gritter.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
